994f2adeb96f195b742d6443229786c23208e66f
Patrow1 <patrow1@o2.pl>
2025-03-09 12:58:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,5 @@ function registerUser() {
     } else {
         alert("Wypełnij wszystkie pola!");
     }
-[
-    {
-        "email": "admin@example.com",
-        "password": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd53df8b705d7c7c5d3"
-    }
-]
+
 
