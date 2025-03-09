7d62e511c4b246e71e54090490ce681fe99c816a
Patrow1 <patrow1@o2.pl>
2025-03-09 12:41:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function showMessage() {
     alert("Dziękujemy za odwiedziny naszej drukarni!");
 }
+document.getElementById("loginBtn").addEventListener("click", function() {
+    document.getElementById("registerForm").classList.toggle("hidden");
+});
+
+function registerUser() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
+    if (email && password) {
+        alert("Rejestracja zakończona!");
+    } else {
+        alert("Wypełnij wszystkie pola!");
+    }
+}
